688b32e1a1119b4c5a6f56da016193be3b768d69
robabby <robabby23@gmail.com>
2017-09-02 17:09:22 -0500

    Google Oauth

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+// Auth needs
+const passport = require('passport');
+// http://console.developers.google.com to set up a new Oauth app
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'world' });
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    // once you hit this segment of the process, the token is cached
+    // and google wont need to ask for permission again until expiration
+    console.log('access token: ', accessToken);
+    console.log('refresh token: ', refreshToken);
+    console.log('profile: ', profile);
+  })
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
