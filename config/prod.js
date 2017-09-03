3b5167937d05f077bc6c299f001e469aced1c053
robabby <robabby23@gmail.com>
2017-09-02 21:31:00 -0500

    Prod and dev env

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // robabby83@gmail.com
+  // https://console.developers.google.com/apis/dashboard?project=nootristack-prod&duration=PT1H
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGU_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
