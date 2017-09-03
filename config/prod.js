f3a995976b26d5fc1fddeb3b0fd5640edb9b36c4
robabby <robabby23@gmail.com>
2017-09-02 21:34:21 -0500

    Fixed typo

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,6 @@ module.exports = {
   // https://console.developers.google.com/apis/dashboard?project=nootristack-prod&duration=PT1H
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGU_URI,
+  mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 }
