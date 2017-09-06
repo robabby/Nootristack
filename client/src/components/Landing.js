961c0e2e2881a844ec9be1519677255e38a8e0f7
robabby <robabby23@gmail.com>
2017-09-05 21:04:28 -0500

    Header and nav completed

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Nootristack</h1>
+      Organize and budget your Nootropic supplements!
+    </div>
+  )
+}
+
+export default Landing;
