5365414a566ee0f2ab2db6b5f843e1c80f37010b
robabby <robabby23@gmail.com>
2018-05-12 18:20:16 -0500

    Revert "Moved"
    This reverts commit ea29f7744974cfe79745917dbc6b31bdc88c830e.

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => async (dispatch) => {
+  // if an action creator returns a function instead of an action
+  // redux-thunk will automatically call the return function
+  // and pass in the /dispatch/ function as an argument
+  const res = await axios.get('/api/current_user')
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
---
+++ client/src/components/Header.js
@@ -0,0 +1,44 @@
+import React,  { Component } from 'react';
+// hooking up to the reduc store and connect to state
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        );
+      default:
+        return <li><a href="/api/logout">Log Out</a></li>;
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'} 
+            className="brand-logo left"
+          >
+            Nootristack
+          </Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+// { auth } is destructured out of /state/
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
