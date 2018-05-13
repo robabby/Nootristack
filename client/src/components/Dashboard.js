6d58cf5c51c2da2bb8a76a594b67176aa8626415
robabby <robabby23@gmail.com>
2018-05-12 21:32:51 -0500

    Create new stack page

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
     <div>
+      Dashboard
       <div className="fixed-action-btn">
-        <a className="btn-floating btn-large red">
+        <Link
+          to="/stacks/new"
+          className="btn-floating btn-large red"
+        >
           <i className="material-icons">add</i>
-        </a>
+        </Link>
       </div>
     </div>
   );
