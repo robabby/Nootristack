cb1dd9d2af9e1e49d4e15bcaaf25ced18c8ca9b6
robabby <robabby23@gmail.com>
2018-05-12 19:26:59 -0500

    catchall

--- index.js
+++ index.js
@@ -22,5 +22,16 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve up production assets
+  app.use(express.static('client/build'));
+
+  // Serve index.html if unrecognized route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
