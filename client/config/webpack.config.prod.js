ea570bb0fc15762eb9ebe993555516f1cec6a394
robabby <robabby23@gmail.com>
2018-05-15 00:30:17 -0500

    scss

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -167,53 +167,25 @@ module.exports = {
           // in the main CSS file.
           {
             test: /\.scss$/,
-            loader: ExtractTextPlugin.extract(
-              Object.assign(
-                {
-                  fallback: {
-                    loader: require.resolve('style-loader'),
-                    options: {
-                      hmr: false,
-                    },
-                  },
-                  use: [
-                    {
-                      loader: require.resolve('css-loader'),
-                      options: {
-                        importLoaders: 1,
-                        minimize: true,
-                        sourceMap: shouldUseSourceMap,
-                      },
-                    },
-                    {
-                      loader: require.resolve('sass-loader'),
-                    },
-                    {
-                      loader: require.resolve('postcss-loader'),
-                      options: {
-                        // Necessary for external CSS imports to work
-                        // https://github.com/facebookincubator/create-react-app/issues/2677
-                        ident: 'postcss',
-                        plugins: () => [
-                          require('postcss-flexbugs-fixes'),
-                          autoprefixer({
-                            browsers: [
-                              '>1%',
-                              'last 4 versions',
-                              'Firefox ESR',
-                              'not ie < 9', // React doesn't support IE8 anyway
-                            ],
-                            flexbox: 'no-2009',
-                          }),
-                        ],
-                      },
-                    },
-                  ],
-                },
-                extractTextPluginOptions
-              )
-            ),
-            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+            include: [paths.appSrc, paths.appNodeModules],
+            use: [
+              {
+                loader: require.resolve('style-loader'),
+                options: {
+                  sourceMap: shouldUseSourceMap
+                }
+              },
+              {
+                loader: require.resolve('css-loader'),
+              },
+              require.resolve('resolve-url-loader'),
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  sourceMap: shouldUseSourceMap
+                }
+              }
+            ]
           },
           {
             test: /\.css$/,
