cb2a416709799a7aab07938a1c80acb5cfc15f25
robabby <robabby23@gmail.com>
2018-05-21 07:58:16 -0500

    cleanup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,4 @@
-import  { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 import stacksReducer from './stacksReducer';
